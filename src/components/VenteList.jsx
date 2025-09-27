ef6584ffa4c67fb9cfe508ec543d952cd1c88e97
AyouBattle <152877288+AyouBattle@users.noreply.github.com>
2025-09-27 03:05:35 +0200

    css

--- src/components/VenteList.jsx
+++ src/components/VenteList.jsx
@@ -11,7 +11,7 @@ function VenteList({ ventes, onModifier, onSupprimer }) {
             {vente.quantite * vente.prix}€{" "}
             <button onClick={() => onModifier(vente)}>✏️</button>
             <button onClick={() => onSupprimer(vente.id)}>🗑️</button>
-            <button onClick={() => onSupprimer(vente.id)} style={{backgroundColor: "green", color: "black"}}>X</button>
+            <button onClick={() => onSupprimer(vente.id)} style={{backgroundColor: "green", color: "blue"}}>X</button>
           </li>
         ))}
       </ul>
